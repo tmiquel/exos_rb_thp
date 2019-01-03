6a9cb4a2fc388084f5728c1ffc2d7383f3d89c40
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-01-03 18:53:44 +0100

    Exos de jeudi

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, c'est quoi ton prénom ?"
+print "> "
+user_name = gets.chomp
+puts "Bonjour, c'est quoi ton nom de famille ?"
+print "> "
+user_family_name = gets.chomp
+puts "Bonjour, #{user_name} #{user_family_name}!"
